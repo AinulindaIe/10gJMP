a2b06fbe9688415ddb1144e1be0a03a6f6ea9a12
Mads Christensen <mads.bchrist@gmail.com>
2018-12-17 21:51:55 +0100

    Har prøvet at lave shufflelist og shuffle funktioner. Det virker ikke

--- DiverseIdeer.fsx
+++ DiverseIdeer.fsx
@@ -1,4 +1,5 @@
-let mutable action = "Default"
+
+  let mutable action = "Default"
     this.updateReproduction ()
     this.updateHunger ()
 
@@ -90,3 +91,43 @@ let availablePos (p : position option) (a : 'a) (b : board) =
         this.board.wolves <- wol :: this.board.wolves
       | None      ->
     | None    -> printf "wolf died"
+
+/// intet af det her under virker.
+
+// let rnd = System.Random()
+
+// let swap (arr: 'a array) (x:int) (y:int) =
+//   let mutable _tmp = arr.[x]
+//   arr.[x] <- arr.[y]
+//   arr.[y] <- _tmp
+
+// let randList (lst:'a list) =
+//   let _arr = List.toArray lst
+//   Array.iteri (fun i _ -> swap _arr i (rnd.Next(i, Array.length _arr))) _arr
+
+// let listing = ["w";"w";"w";"m";"m";"m"]
+
+// printfn "%A" (randList listing)
+
+let rand = new System.Random()
+
+let swap (a: _[]) x y =
+    let tmp = a.[x]
+    a.[x] <- a.[y]
+    a.[y] <- tmp
+
+// shuffle an array (in-place)
+let shuffleArray a : 'a array =
+    printfn "shuffle arr: %A" a
+    Array.iteri (fun i _ -> swap a i (rand.Next(i, Array.length a))) a
+    let newArr = a
+    newArr    
+
+let shuffleList (lst : 'a list) =
+    shuffleArray (lst |> List.toArray)
+
+let lsting = ["w";"w";"w";"m";"m";"m"]
+let listing2 = [|"w";"w";"w";"m";"m";"m"|]
+
+printfn "%A" (shuffleArray listing2) 
+printfn "%A" (shuffleList lsting) 
